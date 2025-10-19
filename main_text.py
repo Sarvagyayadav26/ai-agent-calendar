aa9b6f90e60580e68b47cf7f03820baca6a63847
Sarvagya Yadav <88796527+Sarvagyayadav26@users.noreply.github.com>
2025-10-19 15:28:19 +0530

    Update main_text.py

--- main_text.py
+++ main_text.py
@@ -23,13 +23,6 @@ ai_agent = AIAgent(calendar_service)
 @app.route('/')
 def index():
     return render_template("text.html")
-
-# @app.route('/interact', methods=['POST'])
-# def interact():
-#     data = request.get_json()
-#     text_input = data.get('text_input', '')
-#     response_text = ai_agent.interact(text_input)
-#     return jsonify({'text_response': response_text})
 @app.route('/interact', methods=['POST'])
 def interact():
     data = request.get_json()
@@ -44,5 +37,6 @@ if __name__ == '__main__':
     # Open the browser after a short delay to ensure the server is running
     Timer(1, lambda: webbrowser.open(url)).start()
     app.run(debug=True, port=port)
+
 
 
